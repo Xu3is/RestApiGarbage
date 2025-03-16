6dce889b8319b1fa55b5ed9b7faf002551f476fe
Xu3is <mleliukov@yandex.ru>
2025-03-16 14:45:33 +0300

    graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	todo "github.com/Xu3is/RestApiGarbage"
 	"github.com/Xu3is/RestApiGarbage/pkg/handler"
 	"github.com/Xu3is/RestApiGarbage/pkg/repository"
@@ -10,6 +11,8 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func initConfig() error {
@@ -45,8 +48,23 @@ func main() {
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatalf("error occured while running http server %s", err.Error())
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("failed to initialize server: %s", err.Error())
+		}
+	}()
+	logrus.Print("Application started")
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("Application stopped")
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Fatalf("error occured on server shutting down: %s", err.Error())
+	}
+
+	if err := db.Close(); err != nil {
+		logrus.Fatalf("error occure on db connection close %s", err.Error())
 	}
 
 }
