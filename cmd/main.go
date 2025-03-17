ca03bedad6111729d251d6b56f6a6db692af4bc9
Xu3is <mleliukov@yandex.ru>
2025-03-17 20:03:26 +0300

    inited configs

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,28 @@ import (
 	"github.com/Xu3is/RestApiGarbage/pkg/handler"
 	"github.com/Xu3is/RestApiGarbage/pkg/repository"
 	"github.com/Xu3is/RestApiGarbage/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
+func configInit() error {
+	viper.AddConfigPath("cfgs")
+	viper.SetConfigName("cfg")
+	return viper.ReadInConfig()
+}
+
 func main() {
+	err := configInit()
+	if err != nil {
+		log.Fatalf("Error reading config file, %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(restapigarbage.Server)
-	err := srv.Run("8000", handlers.InitRoutes())
+	err = srv.Run(viper.GetString("port"), handlers.InitRoutes())
 	if err != nil {
 		log.Fatalf("error occured while starting http server: %v", err.Error())
 	}
